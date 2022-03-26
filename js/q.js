b66690d71cdf947ba769b53c95532350034a9f8b
AryaDewata <90488518+AryaDewata@users.noreply.github.com>
2022-03-26 11:33:59 +0800

    Update q.js

--- js/q.js
+++ js/q.js
@@ -1,11 +1,3 @@
-const BULAN = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-function timeConvert(num){
-   let time = new Date(String(num).slice(0, 10) * 1000);
-   let tanggal = time.getDate();
-   let bulan = BULAN[time.getMonth()];
-   let tahun = time.getFullYear();
-   return `${tanggal} ${bulan} ${tahun}`;
-}
 let modalDetail = document.querySelector('#modalDetailNft .modal-dialog .modal-content .modal-body');
 let modalStatistik = document.querySelector('#modalStatistik .modal-dialog .modal-content .modal-body');
 async function getData(url, linkNft){
@@ -69,44 +61,3 @@ btnDetail.forEach(e => {
       getData(`${this.dataset.urlnft}`, `${this.dataset.atomurl}`);
    })
 })
-let formatter = new Intl.NumberFormat('en-US', {
-   style: 'currency',
-   currency: 'USD',
-});
-function toDigit(angka){
-   return formatter.format(String(angka)).split('').slice(1, -3).join('').split(',').join('.');
-}
-fetch('https://wax.api.atomicassets.io/atomicmarket/v1/stats/graph?symbol=WAX&collection_whitelist=samuraiworld')
-.then(response => response.json())
-.then(response => {
-      let dataVolume = [];
-      response.data.results.forEach(e => {
-         let waktu = timeConvert(e.time);
-         let totalPenjualan = e.sales;
-         let volume = Math.ceil(e.volume / 100000000);
-         let data = `
-            <tr>
-               <td>${waktu}</td>
-               <td>${totalPenjualan}</td>
-               <td>${volume} WAX</td>
-            </tr>`
-         dataVolume.push(data);
-      })
-      modalStatistik.innerHTML = `
-      <table class="table table-striped">
-         <tr>
-            <th>Time</th>
-            <th>Sales</th>
-            <th>Volume</th>
-         </tr>
-         ${dataVolume.join('')}
-      </table>`
-      let dayVolume = Math.ceil(response.data.results[response.data.results.length - 1].volume / 100000000);
-      document.querySelector('#dayVol').innerHTML = `<h5 style="color: white"><i class="fas fa-chart-line" style="color: lightgreen"></i> 24h Volume : <span style="color: lightgreen">${dayVolume} WAX</span></h5>`;
-   });
-fetch('https://wax.api.atomicassets.io/atomicmarket/v1/stats/sales?symbol=WAX&collection_whitelist=samuraiworld')
-   .then(response => response.json())
-   .then(response => {
-      let totalVolume = parseInt(response.data.result.volume / 100000000);
-      document.querySelector('#totalVol').innerHTML = `<h5 style="color: white"><i class="fas fa-chart-line" style="color: lightgreen"></i> Total Volume : <span style="color: lightgreen">${totalVolume} WAX</span></h5>`;
-   });
